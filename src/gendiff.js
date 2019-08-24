27855f841bcd9bf4978da70a6d03111485ddc9b8
KhrebtovD <khrebtov.dmitriy@gmail.com>
2019-08-25 01:38:33 +0400

    step 2

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,5 +5,8 @@ export default () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action()
     .parse(process.argv);
 };
