ee27629816dc22958f70b36b83a4d5ab15f49f34
KhrebtovD <khrebtov.dmitriy@gmail.com>
2019-08-25 16:58:24 +0400

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
-import gendiff from '../gendiff';
+import commander from 'commander';
+import genDiff from '../gendiff';
 
-gendiff();
+const program = new commander.Command();
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action(genDiff)
+  .parse(process.argv);
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,12 +1,48 @@
-import commander from 'commander';
+import fs from 'fs';
 
-export default () => {
-  const program = commander;
-  program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<firstConfig> <secondConfig>')
-    .action()
-    .parse(process.argv);
+const genDiff = (pathToFile1, pathToFile2) => {
+  const makeSort = (a, b) => {
+    const key1 = a[1][0];
+    const key2 = b[1][0];
+    if (key1 > key2) {
+      return 1;
+    }
+    if (key1 < key2) {
+      return -1;
+    }
+    return 0;
+  };
+
+  const mergeIdenticProps = (array) => {
+    const isSame = (a, b) => a[1] === b[1] && a[2] === b[2];
+    const iter = (arr, acc) => {
+      if (arr.length === 0) {
+        return acc;
+      }
+      const [first, second] = arr;
+      if (arr.length === 1) {
+        return [...acc, first];
+      }
+      const marker = ' ';
+      const newElem = isSame(first, second) ? [marker, first[1], first[2]] : first;
+      const rest = isSame(first, second) ? arr.slice(2) : arr.slice(1);
+      return iter(rest, [...acc, newElem]);
+    };
+    return iter(array, []);
+  };
+
+  const getData = path => JSON.parse(fs.readFileSync(path, 'utf8'));
+  const buildArray = (data, marker) => Object.entries(data).map(prop => [marker, ...prop]);
+  const data1 = buildArray(getData(pathToFile1), '-');
+  const data2 = buildArray(getData(pathToFile2), '+');
+  const commonArr = [...data1, ...data2];
+  const sortedWithIdentic = mergeIdenticProps(commonArr.sort(makeSort));
+  const body = sortedWithIdentic
+    .map(e => [e[0], `${e[1]}:`, e[2]])
+    .map(e => e.join(' ')).join('\n  ');
+  const result = `{\n  ${body}\n}`;
+  console.log(result);
+  return result;
 };
+
+export default genDiff;
